dfae0b42250dfb49242923cc33b9b662f31137b3
Schoonology <michael.r.schoonmaker@gmail.com>
2017-03-06 14:05:03 -0800

    Update util from ZMTP.

--- src/util.cpp
+++ src/util.cpp
@@ -8,3 +8,24 @@ void __debug_dump (const char *label, const uint8_t *buffer, uint8_t len) {
   }
   Serial.println ();
 }
+
+void zero_bytes(uint8_t *buffer, uint8_t offset, uint8_t length) {
+  for (uint8_t i = offset; i < (offset + length); i++) {
+    buffer[i] = 0;
+  }
+}
+
+uint8_t *uuid_new () {
+  uint8_t * uuid = (uint8_t *) malloc (16);
+  assert (uuid);
+
+  for (uint8_t i = 0; i < 16; ++i) {
+    uuid[i] = random (256);
+  }
+
+  return uuid;
+}
+
+bool uuid_equal (uint8_t *first, uint8_t *second) {
+  return !memcmp (first, second, 16);
+}
--- src/util.h
+++ src/util.h
@@ -17,8 +17,20 @@
   ((void) Serial.printf ("%s:%u: failed assertion '%s'\n", file, line, e), \
     abort ())
 
-#define debug_dump(e, len) \
+#define VAR_ARG_MACRO(_1,_2,NAME,...) NAME
+#define debug_dump(...) VAR_ARG_MACRO(__VA_ARGS__, debug_dump2, debug_dump1)(__VA_ARGS__)
+
+#define debug_dump1(e) \
+  ((void) (Serial.printf("%s = ", #e) && Serial.println(e) ))
+
+#define debug_dump2(e, len) \
   ((void) __debug_dump (#e, e, len))
 void __debug_dump (const char *label, const uint8_t *buffer, uint8_t len);
+
+void zero_bytes(uint8_t *buffer, uint8_t offset, uint8_t length);
+
+uint8_t *uuid_new ();
+
+bool uuid_equal (uint8_t *first, uint8_t *second);
 
 #endif
